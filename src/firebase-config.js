1c684dd863468519af34c46b87f2cbea6029b459
shubham0794x <shubham07bhadani@gmail.com>
2022-02-01 19:57:57 +0530

    Firebase Crud App

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDuRWEhVWErVncQ-PZasy1Zl_sLyMiLCdg",
+    authDomain: "fir-crud-d74ee.firebaseapp.com",
+    projectId: "fir-crud-d74ee",
+    storageBucket: "fir-crud-d74ee.appspot.com",
+    messagingSenderId: "162283752508",
+    appId: "1:162283752508:web:c494f7e34e1c4a9c8c4739",
+    measurementId: "G-NQ1X8S2WMY"
+  };
+
+  const app = initializeApp(firebaseConfig);
