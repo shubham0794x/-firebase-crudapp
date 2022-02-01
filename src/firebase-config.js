20fe60f9ab60c64b6c560bc071afec69c9cb780f
shubham0794x <shubham07bhadani@gmail.com>
2022-02-01 20:26:51 +0530

    Firebase Crud Application V1

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyDuRWEhVWErVncQ-PZasy1Zl_sLyMiLCdg",
@@ -11,3 +13,5 @@ const firebaseConfig = {
   };
 
   const app = initializeApp(firebaseConfig);
+
+  export const db = getFirestore(app);
